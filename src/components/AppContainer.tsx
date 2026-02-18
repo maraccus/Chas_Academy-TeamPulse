ce2244ac789e746a1b192fb9ec0ad4f0de346cb1
Marcus <marcuswilliamj@gmail.com>
2026-02-18 13:31:16 +0100

    Project init

---
+++ src/components/AppContainer.tsx
@@ -0,0 +1,17 @@
+import React, {ReactNode} from 'react'
+import styles from "./AppContainer.module.css"
+
+type AppContainerProps = {
+    children: ReactNode
+}
+
+const AppContainer = ({children}: AppContainerProps) => {
+  return (
+    <div className={styles.container}>
+        {/* <h1>Cååntainer</h1> */}
+        {children}
+    </div>
+  )
+}
+
+export default AppContainer
